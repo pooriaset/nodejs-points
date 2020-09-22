34ec480d03bd3e2c99b0a819d4c4f33c06b85c85
Pooria <51507043+pooriaset@users.noreply.github.com>
2020-09-22 21:44:14 +0330

    Add MapController

---
+++ controllers/MapController.js
@@ -0,0 +1,99 @@
+const mongoose = require("mongoose");
+const Map = require("../models/map");
+const ObjectId = mongoose.Types.ObjectId;
+
+class MapController {
+    async getById(req, res) {
+        try {
+            let map = await Map.findById(req.params.id);
+            res.status(200).json(map);
+        }
+        catch (error) {
+            res.status(500).json({ message: "خطای ناشناخته رخ داده است!", status: 500 });
+        }
+    }
+
+    async getByPage(req, res) {
+        const pagesize = 2;
+        const page = req.params.page;
+        try {
+            let maps = await Map.find().skip(pagesize * (page - 1)).limit(pagesize);
+            if (!maps.length) {
+                res.status(500).json({
+                    message: "موردی یافت نشد",
+                    status: 500
+                });
+            }
+            res.status(200).json({
+                data: maps,
+                status: 200,
+                message: "با موفقیت به روز شد"
+            });
+        }
+        catch (error) {
+            res.json({
+                message: "خطای ناشناخته رخ داده است!",
+                status: 500
+            });
+        }
+    }
+
+    async createMap(req, res) {
+        try {
+            let newMap = new Map({
+                latitude: req.body.latitude,
+                longitude: req.body.longitude,
+                address: req.body.address,
+                phone: req.body.phone,
+                userid: ObjectId(req.user.id)
+            });
+
+            await newMap.save();
+            res.status(201).json({ message: "با موفقیت ثبت شد!", status: 201 });
+        }
+        catch (error) {
+            console.log(error.message);
+            res.status(500).json({ message: "خطای ناشناخته رخ داده است!", status: 500 });
+        }
+
+    }
+
+    async updateOne(req, res) {
+        try {
+
+            await Map.updateOne({ _id: req.params.id }, { $set: req.body });
+
+            res.status(200).json({
+                message: "با موفقیت ویرایش شد!",
+                status: 200
+            });
+        }
+        catch (error) {
+            console.log(error.message);
+            res.status(500).json({
+                message: "خطای ناشناخته رخ داده است!",
+                status: 500
+            });
+        }
+    }
+
+    async deleteOne(req, res) {
+        try {
+            await Map.deleteOne({ _id: req.params.id });
+            res.status(200).json({
+                message: "با موفقیت حذف شد!",
+                status: 200
+            });
+
+        } catch (error) {
+            console.log(error.message);
+            res.status(500).json({
+                message: "خطای ناشناخته رخ داده است!",
+                status: 500
+            });
+        }
+    }
+}
+
+
+module.exports = new MapController;
--- routes/map.js
+++ routes/map.js
@@ -2,108 +2,27 @@
 const express = require("express");
 const router = express.Router();
 
-//Database tools
-const mongoose = require("mongoose");
-const Map = require("../models/map");
-const ObjectId = mongoose.Types.ObjectId;
+//import controller
+const MapController = require("../controllers/MapController");
 
 //Validation config
 const authorization = require("../validations/authorization");
 const checkMapOwnership = require("../validations/checkMapOwnership");
 
 //GET (get single map by id)
-router.get("/:id", async (req, res) => {
-    try {
-        let map = await Map.findById(req.params.id);
-        res.status(200).json(map);
-    }
-    catch (error) {
-        res.status(500).json({ message: "خطای ناشناخته رخ داده است!", status: 500 });
-    }
-});
+router.get("/:id", MapController.getById);
 
 //GET (get map by page)
-router.get("/page/:page", async (req, res) => {
-    const pagesize = 2;
-    const page = req.params.page;
-    try {
-        let maps = await Map.find().skip(pagesize * (page - 1)).limit(pagesize);
-        if (!maps.length) {
-            res.status(500).json({
-                message: "موردی یافت نشد",
-                status: 500
-            });
-        }
-        res.status(200).json({
-            data: maps,
-            status: 200,
-            message: "با موفقیت به روز شد"
-        });
-    }
-    catch (error) {
-        res.json({
-            message: "خطای ناشناخته رخ داده است!",
-            status: 500
-        });
-    }
-});
+router.get("/page/:page", MapController.getByPage);
 
 //POST (create new map)
-router.post("/create", authorization, async (req, res) => {
-    try {
-        let newMap = new Map({
-            latitude: req.body.latitude,
-            longitude: req.body.longitude,
-            address: req.body.address,
-            phone: req.body.phone,
-            userid: ObjectId(req.user.id)
-        });
-
-        await newMap.save();
-        res.status(201).json({ message: "با موفقیت ثبت شد!", status: 201 });
-    }
-    catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: "خطای ناشناخته رخ داده است!", status: 500 });
-    }
-
-});
+router.post("/create", authorization, MapController.createMap);
 
 //PUT (update a map data)
-router.put("/update:id", authorization, checkMapOwnership, async (req, res) => {
-    try {
-
-        await Map.updateOne({ _id: req.params.id }, { $set: req.body });
-
-        res.status(200).json({
-            message: "با موفقیت ویرایش شد!",
-            status: 200
-        });
-    }
-    catch (error) {
-        console.log(error.message);
-        res.status(500).json({
-            message: "خطای ناشناخته رخ داده است!",
-            status: 500
-        });
-    }
-});
+router.put("/update:id", authorization, checkMapOwnership, MapController.updateOne);
 
 //Delete (delete a map)
-router.delete("/delete/:id", authorization, checkMapOwnership, async (req, res) => {
-    try {
-        await Map.deleteOne({ _id: req.params.id});
-        res.status(200).json({
-            message: "با موفقیت حذف شد!",
-            status: 200
-        });
+router.delete("/delete/:id", authorization, checkMapOwnership, MapController.deleteOne);
 
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({
-            message: "خطای ناشناخته رخ داده است!",
-            status: 500
-        });
-    }
-});
+
 module.exports = router;
