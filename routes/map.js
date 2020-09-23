f77793251e03812b441876db8dd692f1e939dc2c
Pooria <51507043+pooriaset@users.noreply.github.com>
2020-09-23 18:51:03 +0330

    Add MapValidator.js

--- routes/map.js
+++ routes/map.js
@@ -8,7 +8,7 @@ const MapController = require("../controllers/MapController");
 //Validation config
 const authorization = require("../validations/authorization");
 const checkMapOwnership = require("../validations/checkMapOwnership");
-const { body } = require("express-validator");
+const mapValidator = require("../validations/mapValidator");
 
 //GET (get single map by id)
 router.get("/:id", MapController.getById);
@@ -17,26 +17,10 @@ router.get("/:id", MapController.getById);
 router.get("/page/:page", MapController.getByPage);
 
 //POST (create new map)
-router.post("/create", authorization,
-    [
-        body('latitude', "لتیتود باید از جنس عدد باشد").isNumeric(),
-        body('longitude', "لانگیتود باید از جنس عدد باشد").isNumeric(),
-        body('address', "آدرس باید حداقل ۵ کاراکتر باشد").trim().isString().isLength({ min: 5 }),
-        body('phone', "تلفن نباید خالی باشد").trim().notEmpty(),
-        body('phone', "تلفن باید حداکثر ۱۱ کاراکتر باشد").trim().isLength({ max: 11 })
-    ]
-    , MapController.createMap);
+router.post("/create", authorization, mapValidator.create(), MapController.createMap);
 
 //PUT (update a map data)
-router.put("/update/:id", authorization,
-    [
-        body('latitude', "لتیتود باید از جنس عدد باشد").isNumeric(),
-        body('longitude', "لانگیتود باید از جنس عدد باشد").isNumeric(),
-        body('address', "آدرس باید حداقل ۵ کاراکتر باشد").trim().isString().isLength({ min: 5 }),
-        body('phone', "تلفن نباید خالی باشد").trim().notEmpty(),
-        body('phone', "تلفن باید حداکثر ۱۱ کاراکتر باشد").trim().isLength({ max: 11 })
-    ], checkMapOwnership,
-     MapController.updateOne);
+router.put("/update/:id", authorization, mapValidator.update(), checkMapOwnership, MapController.updateOne);
 
 //Delete (delete a map)
 router.delete("/delete/:id", authorization, checkMapOwnership, MapController.deleteOne);
