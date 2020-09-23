22a84a2c81afbb77b4b5085066dfaf4cb3c0e063
Pooria <51507043+pooriaset@users.noreply.github.com>
2020-09-23 18:13:17 +0330

    Add validaton to updade map

--- routes/map.js
+++ routes/map.js
@@ -8,7 +8,7 @@ const MapController = require("../controllers/MapController");
 //Validation config
 const authorization = require("../validations/authorization");
 const checkMapOwnership = require("../validations/checkMapOwnership");
-const { body } = require("express-validator/check");
+const { body } = require("express-validator");
 
 //GET (get single map by id)
 router.get("/:id", MapController.getById);
@@ -17,19 +17,26 @@ router.get("/:id", MapController.getById);
 router.get("/page/:page", MapController.getByPage);
 
 //POST (create new map)
-router.post("/create",
-    authorization,
+router.post("/create", authorization,
     [
-        body('latitude' , "لتیتود باید از جنس عدد باشد").isNumeric(),
+        body('latitude', "لتیتود باید از جنس عدد باشد").isNumeric(),
         body('longitude', "لانگیتود باید از جنس عدد باشد").isNumeric(),
         body('address', "آدرس باید حداقل ۵ کاراکتر باشد").trim().isString().isLength({ min: 5 }),
         body('phone', "تلفن نباید خالی باشد").trim().notEmpty(),
         body('phone', "تلفن باید حداکثر ۱۱ کاراکتر باشد").trim().isLength({ max: 11 })
-    ],
-    MapController.createMap);
+    ]
+    , MapController.createMap);
 
 //PUT (update a map data)
-router.put("/update/:id", authorization, checkMapOwnership, MapController.updateOne);
+router.put("/update/:id", authorization,
+    [
+        body('latitude', "لتیتود باید از جنس عدد باشد").isNumeric(),
+        body('longitude', "لانگیتود باید از جنس عدد باشد").isNumeric(),
+        body('address', "آدرس باید حداقل ۵ کاراکتر باشد").trim().isString().isLength({ min: 5 }),
+        body('phone', "تلفن نباید خالی باشد").trim().notEmpty(),
+        body('phone', "تلفن باید حداکثر ۱۱ کاراکتر باشد").trim().isLength({ max: 11 })
+    ], checkMapOwnership,
+     MapController.updateOne);
 
 //Delete (delete a map)
 router.delete("/delete/:id", authorization, checkMapOwnership, MapController.deleteOne);
