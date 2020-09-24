62a30009bd5b645be02204d359e6f94f697ab08f
Pooria <51507043+pooriaset@users.noreply.github.com>
2020-09-24 19:52:57 +0330

    Refact map model

--- models/map.js
+++ models/map.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
-const ObjectId = mongoose.Schema.ObjectId;
 
 const mapSchema = mongoose.Schema({
-    latitude: { type: Number, required : true},
-    longitude: { type: Number, required : true},
-    address: { type: String, required : true},
-    phone: { type: String},
-    userid: { type: ObjectId, required : true},
-    date: { type: Date, required : true , default : new Date()}
+    latitude: { type: Number, required: true },
+    longitude: { type: Number, required: true },
+    address: { type: String, required: true },
+    phone: { type: String },
+    userid: { type: mongoose.Types.ObjectId, ref: "user", required: true },
+    date: { type: Date, required: true, default: new Date() }
 });
 
-module.exports = mongoose.model("map" , mapSchema);
+module.exports = mongoose.model("map", mapSchema);
