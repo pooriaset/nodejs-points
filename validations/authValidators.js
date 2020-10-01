b1de6c54317de684375b77c3c9e26d3d00566f13
Pooria <51507043+pooriaset@users.noreply.github.com>
2020-10-01 09:58:41 +0330

    Debug XSS security vulnerability!

--- validations/authValidators.js
+++ validations/authValidators.js
@@ -2,17 +2,18 @@ const { body } = require("express-validator");
 class AuthValidators {
     loginValidation = () => {
         return [
-            body("username", "نام کاربری وارد نشده است").notEmpty(),
-            body("password", "رمز عبور وارد نشده است").notEmpty()
+            body("username", "نام کاربری وارد نشده است").notEmpty().escape(),
+            body("password", "رمز عبور وارد نشده است").notEmpty().escape()
         ];
     }
 
     registerValidation = () => {
         return [
-            body("username", "نام کاربری وارد نشده است").notEmpty(),
-            body("password", "رمز عبور وارد نشده است").notEmpty(),
-            body("email", "ایمیل وارد نشده است").notEmpty(),
-            body("fullname", "نام و نام خانوادگی وارد نشده است").notEmpty()
+            body("username", "نام کاربری وارد نشده است").notEmpty().escape(),
+            body("password", "رمز عبور وارد نشده است").notEmpty().escape(),
+            body("email", "ایمیل وارد نشده است").notEmpty().escape(),
+            body("email" , "ایمیل به درستی وارد نشده است").normalizeEmail().isEmail().escape(),
+            body("fullname", "نام و نام خانوادگی وارد نشده است").notEmpty().escape()
         ];
     }
 }
--- validations/mapValidator.js
+++ validations/mapValidator.js
@@ -5,18 +5,18 @@ class MapValidator {
         return [
             body('latitude', "لتیتود باید از جنس عدد باشد").isNumeric(),
             body('longitude', "لانگیتود باید از جنس عدد باشد").isNumeric(),
-            body('address', "آدرس باید حداقل ۵ کاراکتر باشد").trim().isString().isLength({ min: 5 }),
-            body('phone', "تلفن نباید خالی باشد").trim().notEmpty(),
-            body('phone', "تلفن باید حداکثر ۱۱ کاراکتر باشد").trim().isLength({ max: 11 })
+            body('address', "آدرس باید حداقل ۵ کاراکتر باشد").trim().escape().isString().isLength({ min: 5 }),
+            body('phone', "تلفن نباید خالی باشد").trim().escape().notEmpty(),
+            body('phone', "تلفن باید حداکثر ۱۱ کاراکتر باشد").trim().escape().isLength({ max: 11 })
         ];
     }
     update = () => {
         return [
             body('latitude', "لتیتود باید از جنس عدد باشد").isNumeric(),
             body('longitude', "لانگیتود باید از جنس عدد باشد").isNumeric(),
-            body('address', "آدرس باید حداقل ۵ کاراکتر باشد").trim().isString().isLength({ min: 5 }),
-            body('phone', "تلفن نباید خالی باشد").trim().notEmpty(),
-            body('phone', "تلفن باید حداکثر ۱۱ کاراکتر باشد").trim().isLength({ max: 11 })
+            body('address', "آدرس باید حداقل ۵ کاراکتر باشد").trim().escape().isString().isLength({ min: 5 }),
+            body('phone', "تلفن نباید خالی باشد").trim().escape().notEmpty(),
+            body('phone', "تلفن باید حداکثر ۱۱ کاراکتر باشد").trim().escape().isLength({ max: 11 })
         ];
     }
 }
